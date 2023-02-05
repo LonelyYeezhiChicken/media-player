c97bec8198eaffa5bd317bfc89ef4b4e2162f79d
chicken <hibary.taiwan@gmail.com>
2023-02-05 22:25:38 +0800

    feat: 下載註冊功能

--- README.md
+++ README.md
@@ -4,3 +4,27 @@
 
 ### 組件依賴
 - YoutubeExplode
+
+### 影音下載
+- 註冊
+```
+ services.AddMediaDownload();
+```
+
+- 介面
+```
+public interface IMediaDownloadProvider
+{
+    /// <summary>
+    /// 下載 youtube
+    /// </summary>
+    /// <param name="url">網址</param>
+    Task YoutubeDownloadAsync(string url);
+
+    /// <summary>
+    /// 取得剛下載的音樂資訊
+    /// </summary>
+    /// <returns>VideoInfoDto 音樂資訊</returns>
+    VideoInfoDto GetYoutubeVideoInfo();
+}
+```
---
+++ src/Media-Player.Download/MediaDownloadProvider.cs
@@ -0,0 +1,49 @@
+﻿using Media_Player.Download.Models;
+using Media_Player.Download.Service;
+
+namespace Media_Player.Download
+{
+    public interface IMediaDownloadProvider
+    {
+        /// <summary>
+        /// 下載 youtube
+        /// </summary>
+        /// <param name="url">網址</param>
+        Task YoutubeDownloadAsync(string url);
+
+        /// <summary>
+        /// 取得剛下載的音樂資訊
+        /// </summary>
+        /// <returns>VideoInfoDto 音樂資訊</returns>
+        VideoInfoDto GetYoutubeVideoInfo();
+    }
+
+    public class MediaDownloadProvider : IMediaDownloadProvider
+    {
+
+        private readonly IYoutubeService _youtubeService;
+
+        public MediaDownloadProvider(IYoutubeService youtubeService)
+        {
+            _youtubeService = youtubeService;
+        }
+
+        /// <summary>
+        /// 下載 youtube
+        /// </summary>
+        /// <param name="url">網址</param>
+        public async Task YoutubeDownloadAsync(string url)
+        {
+            await _youtubeService.DownloadAsync(url);
+        }
+
+        /// <summary>
+        /// 取得剛下載的音樂資訊
+        /// </summary>
+        /// <returns>VideoInfoDto 音樂資訊</returns>
+        public VideoInfoDto GetYoutubeVideoInfo()
+        {
+            return _youtubeService.GetVideoInfo();
+        }
+    }
+}
